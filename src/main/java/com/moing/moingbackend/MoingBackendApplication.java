ec1be66cc62c5ce8b6a63d1f0f882a5ac24ddd3b
milkioiy <93613157+milkioiy@users.noreply.github.com>
2023-09-14 21:04:46 +0900

    setting

---
+++ src/main/java/com/moing/moingbackend/MoingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moing.moingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moing/moingbackend/MoingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moing.moingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
