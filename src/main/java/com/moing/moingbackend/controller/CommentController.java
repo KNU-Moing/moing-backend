19a16db6d2994d640a7d3d733ccfb33f27d5c2b0
onikesum <egg0427@naver.com>
2023-09-18 01:37:53 +0900

    answer CRUD

---
+++ src/main/java/com/moing/moingbackend/controller/CommentController.java
@@ -0,0 +1,65 @@
+package com.moing.moingbackend.controller;
+
+import com.moing.moingbackend.data.dto.CommentDto;
+import com.moing.moingbackend.service.CommentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/Comment")
+public class CommentController {
+    private final CommentService commentService;
+    private final Logger LOGGER = LoggerFactory.getLogger(CommentService.class);
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<CommentDto> createComment(
+            @RequestBody CommentDto commentDto, HttpServletRequest request) {
+        CommentDto commentDtoResponse = commentService.saveComment(commentDto);
+
+        LOGGER.info("호출 API: " + "create Comment" + " 접속자 IP: " + request.getRemoteAddr() + ", 최초 접속 시간: " + LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.OK).body(commentDtoResponse);
+    }
+
+    @GetMapping()
+    public ResponseEntity<CommentDto> getComment(Long id, HttpServletRequest request) {
+        CommentDto commentDtoResponse = commentService.getComment(id);
+
+        LOGGER.info("호출 API: " + "get Comment" + " 접속자 IP: " + request.getRemoteAddr() + ", 최초 접속 시간: " + LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.OK).body(commentDtoResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CommentDto> updateComment(
+            @PathVariable Long id,
+            @RequestParam("answer") String answer, HttpServletRequest request) throws Exception {
+        CommentDto commentDtoResponse = commentService.getComment(id);
+        commentDtoResponse.setAnswer(answer);
+
+        LOGGER.info("호출 API: " + "update Comment" + " 접속자 IP: " + request.getRemoteAddr() + ", 최초 접속 시간: " + LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.OK).body(commentDtoResponse);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<String> deleteComment(Long id, HttpServletRequest request) throws Exception {
+        commentService.deleteComment(id);
+
+        LOGGER.info("호출 API: " + "delete Comment" + " 접속자 IP: " + request.getRemoteAddr() + ", 최초 접속 시간: " + LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.OK).body("성공적으로 삭제 되었습니다.");
+    }
+}
---
+++ src/main/java/com/moing/moingbackend/service/impl/CommentServiceImpl.java
@@ -0,0 +1,79 @@
+package com.moing.moingbackend.service.impl;
+
+import com.moing.moingbackend.data.dao.CommentDAO;
+import com.moing.moingbackend.data.dto.CommentDto;
+import com.moing.moingbackend.data.entity.Comment;
+import com.moing.moingbackend.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentDAO commentDAO;
+    @Autowired
+    public CommentServiceImpl(
+            CommentDAO commentDAO){
+        this.commentDAO = commentDAO;
+    }
+    @Override
+    public CommentDto getComment(Long id) {
+        Comment comment = commentDAO.selectComment(id);
+        if (comment == null) {
+            return null;
+        }
+        CommentDto commentResponse = new CommentDto();
+
+        commentResponse.setId(comment.getId());
+        commentResponse.setAnswer(comment.getAnswer());
+        return commentResponse;
+    }
+
+    @Override
+    public CommentDto saveComment(CommentDto commentDto) {
+        Comment comment = new Comment();
+
+        comment.setId(commentDto.getId());
+        comment.setAnswer(commentDto.getAnswer());
+
+        Comment savedComment = commentDAO.insertComment(comment);
+
+        CommentDto commentResponse = new CommentDto();
+        commentResponse.setId(savedComment.getId());
+        commentResponse.setAnswer(savedComment.getAnswer());
+
+        return commentResponse;
+    }
+
+    @Override
+    public CommentDto updateComment(Long id, CommentDto commentDto) throws Exception {
+        // id로 기존 데이터를 찾아온다.
+        Comment comment = commentDAO.selectComment(id);
+
+        // 기존 데이터가 존재하는지 확인한다.
+        if (comment == null) {
+            throw new Exception("해당 id에 해당하는 데이터가 존재하지 않습니다.");
+        }
+        // 수정할 데이터를 설정한다.
+        comment.setAnswer(commentDto.getAnswer());
+
+        // 수정된 데이터를 데이터베이스에 저장한다.
+        Comment updatedComment = commentDAO.updateComment(comment);
+
+        // 수정된 데이터를 DTO로 변환하여 반환한다.
+        CommentDto updatedCommentDto = new CommentDto();
+        updatedCommentDto.setId(updatedComment.getId());
+        updatedCommentDto.setAnswer(updatedComment.getAnswer());
+        return updatedCommentDto;
+    }
+
+    @Override
+    public void deleteComment(Long id) throws Exception {
+        commentDAO.deleteComment(id);
+    }
+
+    @Override
+    public List<Comment> getAllApplications() {
+        return commentDAO.getAllApplications();
+    }
+}
