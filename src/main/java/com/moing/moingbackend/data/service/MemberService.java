0ccd6cf4a9762107bc374b0496304359078f893b
milkioiy <93613157+milkioiy@users.noreply.github.com>
2023-09-27 02:43:06 +0900

    Complete FindPassword

--- src/main/java/com/moing/moingbackend/data/service/MemberService.java
+++ src/main/java/com/moing/moingbackend/data/service/MemberService.java
@@ -29,7 +29,7 @@ public class MemberService {
         String str = getTempPassword();
         MailDto dto = new MailDto();
         dto.setAddress(email);
-        dto.setTitle("임시비밀번호 안내 이메일 입니다.");
+        dto.setTitle("[모잉] 임시비밀번호 안내 이메일 입니다.");
         dto.setMessage("안녕하세요. 임시비밀번호 안내 관련 이메일 입니다." + " 회원님의 임시 비밀번호는 "
                 + str + " 입니다." + "로그인 후에 비밀번호를 변경을 해주세요");
         updatePassword(str,email);
@@ -37,7 +37,7 @@ public class MemberService {
     }
 
     public void updatePassword(String str, String email){
-        String password = str;
+        String password = passwordEncoder.encode(str);
         Long user_Id = mr.findByEmail(email).getId();
         mmr.updatePassword(user_Id,password);
     }
